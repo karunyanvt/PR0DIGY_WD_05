485d67c02625d933a04afa4e2a11cc039db3dd60
karunyanvt <karunyanvedharethinam@gmail.com>
2025-01-04 00:42:02 +0530

    Added background img

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Weather from './compnents/Weather'
+import backgroundimg from './assets/backgroundimg.jpg'
 
 const App = () => {
   return (
-    <div className='h-screen w-screen flex justify-center items-center p-3'>
+    <div 
+      className="h-screen w-screen flex justify-center items-center p-3" 
+      style={{ backgroundImage: `url(${backgroundimg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
+    >
       <Weather />
     </div>
   )
